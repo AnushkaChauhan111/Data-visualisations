c5af68f65437bf75c1109fbb87346ca309b77db3
Anushka111 <anushkachauhan69532@gmail.com>
2024-07-20 23:19:50 +0530

    Graph of cubic numbers

--- graph.py
+++ graph.py
@@ -1,8 +1,11 @@
 import matplotlib.pyplot as plt
 
-x_values = list(range(1,5001))
+x_values = list(range(1,5))
 y_values = [x**3 for x in x_values]
 fig, ax = plt.subplots()
+ax.set_xlabel("Numbers")
+ax.set_ylabel("Cubic numbers")
+ax.set_title("Number with cubic values")
 ax.plot(x_values,y_values)
 
 plt.show()
