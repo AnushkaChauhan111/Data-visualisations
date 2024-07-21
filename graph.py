59ae6892a114ccd60b0a553d6edfcf12f56d188c
Anushka111 <anushkachauhan69532@gmail.com>
2024-07-21 18:52:52 +0530

    Graph of cubic numbers

--- graph.py
+++ graph.py
@@ -2,10 +2,12 @@ import matplotlib.pyplot as plt
 
 x_values = list(range(1,5))
 y_values = [x**3 for x in x_values]
+plt.style.use('bmh')
+#print(plt.style.available)
 fig, ax = plt.subplots()
 ax.set_xlabel("Numbers")
 ax.set_ylabel("Cubic numbers")
 ax.set_title("Number with cubic values")
-ax.plot(x_values,y_values)
+ax.scatter(x_values,y_values, c=y_values,cmap = "copper",s=100)
 
 plt.show()
