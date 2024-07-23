effaa9f210d15fcf372f8b277b9a813e92bfba48
Anushka111 <anushkachauhan69532@gmail.com>
2024-07-23 22:21:28 +0530

    Correction in Graph of frequency of summ of rolling dice

--- dice_visuals.py
+++ dice_visuals.py
@@ -12,17 +12,18 @@ class Die:
 die_1 = Die(8)
 die_2 = Die(8)
 max_result = die_1.num_sides + die_2.num_sides
+min_result = 2
 sum = []
 for x in range(1000):
     ret = die_1.roll()+die_2.roll()
     sum.append(ret)
 
-rep = []
-for x in range(2,max_result+1):
-    freq = sum.count(x)
-    rep.append(freq)
+frequency = [0]* (max_result - min_result+1)
+for x in sum:
+    if min_result<=x<=max_result:
+        frequency[x-2] += 1
 
-data = [go.Bar(x=sum,y=rep)]
+data = [go.Bar(x=sum,y=frequency)]
 x_axis_config = {'title': 'Result', 'dtick': 1}
 y_axis_config = {'title': 'Frequency of Result'}
 my_layout = go.Layout(title='Results of rolling a D8 and a D8 1000 times',
