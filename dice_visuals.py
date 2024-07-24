13d60f8b21f8f4ea9c67e2f0e6f1f2fbf515995f
Anushka111 <anushkachauhan69532@gmail.com>
2024-07-24 10:44:28 +0530

    Add commit to the file

--- dice_visuals.py
+++ dice_visuals.py
@@ -9,16 +9,16 @@ class Die:
     def roll(self):
         return randint(1,self.num_sides+1)
 
-die_1 = Die(8)
-die_2 = Die(8)
+die_1 = Die(8) #instance 1 of die
+die_2 = Die(8) #instance 2 of die
 max_result = die_1.num_sides + die_2.num_sides
 min_result = 2
-sum = []
+sum = [] # list of sum of all the possible outcomes when rolling 2 d8 die for 1000 times
 for x in range(1000):
     ret = die_1.roll()+die_2.roll()
     sum.append(ret)
 
-frequency = [0]* (max_result - min_result+1)
+frequency = [0]* (max_result - min_result+1) # frequency = list of occurance of each sum
 for x in sum:
     if min_result<=x<=max_result:
         frequency[x-2] += 1
@@ -29,3 +29,4 @@ y_axis_config = {'title': 'Frequency of Result'}
 my_layout = go.Layout(title='Results of rolling a D8 and a D8 1000 times',
  xaxis=x_axis_config, yaxis=y_axis_config)
 plotly.offline.plot({'data': data, 'layout': my_layout}, filename='d8_d8.html')
+
